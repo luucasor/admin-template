ad1f7ca70758bfcccb70ed213f5c7406e8d05f57
luucasor <lucas@jornadatecnologia.com>
2023-07-06 14:50:36 -0300

    Ajustes CSS na tela de login para divisão da tela entre formulário e imagem em diferentes dispositivos

--- src/pages/autenticacao.tsx
+++ src/pages/autenticacao.tsx
@@ -14,11 +14,15 @@ export default function Autenticacao() {
         }
     }
     return (
-        <div className="flex flex-col h-screen items-center justify-center">
-            <div className="w-1/2">
-                <h1 className={`
-                    text-xl font-bold mb-5
-                `}>
+        <div className="flex h-screen items-center justify-center">
+            <div className="hidden md:block md:w-1/2 lg:w-2/3">
+                <img 
+                    src="https://source.unsplash.com/random" 
+                    alt="Imagem da Tela de Autenticação" 
+                    className="h-screen w-full object-cover"/>
+            </div>
+            <div className="m-10 w-full md:w-1/2 lg:w-1/3">
+                <h1 className={`text-3xl font-bold mb-5`}>
                     {modo === 'login' ? 'Entre com a Sua Conta' : 'Cadastre-se na Plataforma'}
                 </h1>
                 <AuthInput 
@@ -51,6 +55,24 @@ export default function Autenticacao() {
                 `}>
                     Entrar com Google
                 </button>
+
+                {modo === 'login' ? (
+                    <p className="mt-8">
+                        Novo por aqui?
+                        <a onClick={() => setModo('cadastro')} className={`
+                            text-blue-500 hover:text-blue-700 font-semibold
+                            cursor-pointer
+                        `}> Crie uma conta Gratuitamente</a>
+                    </p>
+                ) : (
+                    <p className="mt-8">
+                        Já faz parte da nossa comunidade?
+                        <a onClick={() => setModo('cadastro')} className={`
+                            text-blue-500 hover:text-blue-700 font-semibold
+                            cursor-pointer
+                        `}> Entre com as suas Credenciais</a>
+                    </p>
+                )}
             </div>
         </div>
     )
